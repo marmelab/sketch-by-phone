88ee5cf809edc419d8607f3220a04bfdcdc267eb
gildas <gildas.garcia@gmail.com>
2017-06-02 14:55:27 +0200

    [WIP] Gallery of included drawings

---
+++ src/Gallery.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const images = [
+    require('./assets/drawing1.jpg'),
+    require('./assets/drawing2.jpg'),
+    require('./assets/drawing3.jpg'),
+    require('./assets/drawing4.jpg'),
+    require('./assets/drawing5.jpg'),
+    require('./assets/drawing6.jpg'),
+    require('./assets/drawing7.jpg'),
+];
+
+export const Gallery = () => (
+
+)
