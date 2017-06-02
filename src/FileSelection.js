a3de3e633059d9aef746781558375f69abf67859
ThieryMichel <thiery@marmelab.com>
2017-06-02 14:11:17 +0200

    better look for first screen WIP

--- src/FileSelection.js
+++ src/FileSelection.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './FileSelection.css';
 import image from './assets/image.png';
 import getImageDataFromDataUrl from './utils/getImageDataFromDataUrl';
+import hiro from './assets/hiro.png';
 
 class FileSelection extends Component {
     handleChange = (event) => {
@@ -21,19 +22,30 @@ class FileSelection extends Component {
 
     render() {
         return (
-            <div className="fileSelection container-fluid">
-                <div id="form">
-                    <div className="jumbotron">
-                        <h1 className="h4">Sketch by phone</h1>
-                        <hr className="my-4" />
-                        <div className="form-group">
-                            <label htmlFor="file_input">Select a drawing</label>
-                            <input id="file_input" className="form-control-file" type="file" accept="image/*" onChange={this.handleChange} />
+            <div className="file-selection container-fluid">
+                <h1 className="h6 title">Sketch anything you want using your phone as a guide</h1>
+                <hr className="my-4" />
+                <ol>
+                    <li className="hiro">
+                        <div>
+                            Print a <a href={hiro}>hiro marker</a>
                         </div>
-                        <hr className="my-4" />
-                        <button onClick={this.handleClick}>Use the default one</button>
-                    </div>
-                </div>
+                        <div className="marker"><a href={hiro}><img alt="Hiro marker example" src={hiro} /></a></div>
+                    </li>
+                    <li>
+                        Put it on a sheet of paper
+                    </li>
+                    <li>
+                        Choose something to draw
+                        <div className="form-group">
+                            <button htmlFor="file_input">
+                                From your photo library
+                                <input text="hello" id="file_input" className="file-input" type="file" accept="image/*" onChange={this.handleChange} />
+                            </button>
+                        </div>
+                        <button onClick={this.handleClick}>Our drawing</button>
+                    </li>
+                </ol>
             </div>
         );
     }
